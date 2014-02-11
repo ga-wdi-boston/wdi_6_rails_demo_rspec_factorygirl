669f4c75086d3b76e89171d9021729da36248081
Alex Grant <grantovich@users.noreply.github.com>
2014-02-11 09:49:36 -0500

    Add spec for sign-up failure

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 feature 'Visitor signs up' do
-  scenario 'successfully' do
+  background do
     visit root_path
     click_link 'Sign up'
+  end
+
+  scenario 'successfully' do
     fill_in 'Email', with: 'bob@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
@@ -11,5 +14,12 @@ feature 'Visitor signs up' do
 
     expect(page).to have_content 'Signed in as bob@example.com'
     expect(page).to have_link 'Sign out'
+  end
+
+  scenario 'unsuccessfully with blank fields' do
+    click_button 'Sign up'
+
+    expect(page).to_not have_content 'Signed in'
+    expect(page).to have_content "can't be blank"
   end
 end
