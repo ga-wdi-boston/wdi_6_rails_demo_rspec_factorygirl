77c45be142ff791759e269d8f512059d8e2e0ac9
Alex Grant <grantovich@users.noreply.github.com>
2014-02-11 09:43:09 -0500

    Added spec for successful sign-up

---
+++ spec/features/visitor_signs_up_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Visitor signs up' do
+  scenario 'successfully' do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: 'bob@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Signed in as bob@example.com'
+    expect(page).to have_link 'Sign out'
+  end
+end
